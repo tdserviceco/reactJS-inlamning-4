9ab40592d1cab8d4db74baa6888714c3c28504ee
Tommy Danielsson <tdserviceco@gmail.com>
2021-02-18 11:01:13 +0100

    adderade in en loader

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -7,7 +7,8 @@ function Events(props) {
   const
     [events, updateEvents] = useState([{}]),
     [displayError, updateError] = useState(''),
-    [errorIsTrue, updateErrorIsTrue] = useState(true);
+    [errorIsTrue, updateErrorIsTrue] = useState(true),
+    [loading, updateLoading] = useState(true);
 
   const getEvents = async () => {
     return await Axios.get('http://localhost:8080/get/events');
@@ -18,6 +19,7 @@ function Events(props) {
     getEvents().then(res => {
       updateErrorIsTrue(false)
       updateEvents(res.data)
+      updateLoading(false);
     }).catch(error => {
       updateError('Please turn on the API server')
     })
@@ -26,10 +28,16 @@ function Events(props) {
   return (
     <>
       <section className="events-section">
-        {errorIsTrue &&
-          <h1 className="error">{displayError}</h1>
+        {errorIsTrue && loading &&
+          <>
+            <h1 className="error">{displayError}</h1>
+            <div className="loader"></div>
+          </>
         }
-        {!errorIsTrue &&
+        {!errorIsTrue && loading &&
+          <div className="loader"></div>
+        }
+        {!errorIsTrue && !loading &&
           <>
             <h2 className="title">events</h2>
             {events.map((index, key) => <DisplayEvents key={key} value={index} />)}
